e058d1c3a0402a53f49831b852282066cb90c4ce
Feyistu Endale <fayistuendale@gmail.com>
2026-06-24 22:36:50 +0300

    Add Cube class with initialization parameters

---
+++ src/models/cube.py
@@ -0,0 +1,7 @@
+class Cube:
+
+   def __init__(self, position, direction, cube_id):
+       self.position = position
+       self.direction = direction
+       self.id = cube_id
+       self.removed = False
